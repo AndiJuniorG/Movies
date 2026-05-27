e617587a56e3a0d5e07fc562bcee3e53c2a8579a
Andi Gabriel Meneses Segovia <22470058@campeche.tecnm.mx>
2026-05-27 11:27:07 -0600

    Cambio el idioma de la interfaz del usuario

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -5,10 +5,14 @@ namespace Movies.Models
     public class Movie
     {
         public int id { get; set; }
+        [Display (Name = "Titulo")]
         public string? Title { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Feccha de liberación")]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; } 
+        [Display(Name = "Genero")]
+        public string? Genre { get; set; }
+        [Display(Name = "Precio")]
         public decimal Price { get; set; }
 
 
